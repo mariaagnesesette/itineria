aa2cb5560dc68478e2c4864fee5dec6679064774
mariaagnese <sette.maria1@gmail.com>
2026-07-30 09:49:05 +0200

    initial commit

---
+++ src/main/java/com/quattromoschettieri/itineria/ItineriaApplication.java
@@ -0,0 +1,13 @@
+package com.quattromoschettieri.itineria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItineriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItineriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quattromoschettieri/itineria/ItineriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quattromoschettieri.itineria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItineriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
