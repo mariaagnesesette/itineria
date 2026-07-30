309027c5d0c2b2334fe47fab4d15e317961eba2b
mariaagnese <sette.maria1@gmail.com>
2026-07-30 19:08:50 +0200

    spostato enum nella cartella dedicata, adesso è tutto corretto per tutti

--- src/main/java/com/quattromoschettieri/itineria/entities/enums/Tipo.java
+++ src/main/java/com/quattromoschettieri/itineria/entities/enums/Tipo.java
@@ -1,5 +1,11 @@
-package com.quattromoschettieri.itineria.entities.enums;
+package com.quattromoschettieri.itineria.entities;
 
 public enum Tipo {
+
+    MUSEO,
+    BIBLIOTECA,
+    ZONA_VERDE,
+    RISTORANTE,
+    LOCALE
 
 }
