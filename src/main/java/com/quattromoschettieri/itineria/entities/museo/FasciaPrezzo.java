f899ed39db0f7a8cf2ed66ce4f5de637ce366c2a
mariaagnese <sette.maria1@gmail.com>
2026-07-31 11:04:59 +0200

    aggiunta della entity museo e entity correlate ad esso 31/07 ore 11:04

---
+++ src/main/java/com/quattromoschettieri/itineria/entities/museo/FasciaPrezzo.java
@@ -0,0 +1,35 @@
+package com.quattromoschettieri.itineria.entities.museo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "fasce_prezzo")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class FasciaPrezzo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+}
