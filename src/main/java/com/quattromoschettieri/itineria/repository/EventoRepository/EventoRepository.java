605257ce9a70e408f6f997835ba28e1a54a2ae18
mariaagnesesette <sette.maria1@gmail.com>
2026-08-21 11:58:20 +0200

    fix

--- src/main/java/com/quattromoschettieri/itineria/repository/EventoRepository/EventoRepository.java
+++ src/main/java/com/quattromoschettieri/itineria/repository/EventoRepository/EventoRepository.java
@@ -1,4 +1,4 @@
-package com.quattromoschettieri.itineria.repository;
+package com.quattromoschettieri.itineria.repository.EventoRepository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
