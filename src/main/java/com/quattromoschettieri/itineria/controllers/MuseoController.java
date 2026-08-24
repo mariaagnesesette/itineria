21d44fd19dac4a3a292dda79ea03692d15c97f23
Giulia Balaban <balabangiulia8@gmail.com>
2026-08-24 17:57:23 +0200

    collegato siti html

--- src/main/java/com/quattromoschettieri/itineria/controllers/MuseoController.java
+++ src/main/java/com/quattromoschettieri/itineria/controllers/MuseoController.java
@@ -81,7 +81,7 @@ public class MuseoController {
 
         model.addAttribute("museo", risultato);
 
-        return "biblioteche/dettaglio";
+        return "musei/dettaglio";
     }
 
     // ricerca per nome
